628ff058372619fe5a0509a4d57fa1cb62669858
thuhien143 <sakuraace.1403@gmail.com>
2024-07-30 13:49:54 +0700

    fix mobile

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -13,13 +13,13 @@ const MobileNav = ({nav, closeNav}:Props) => {
         const section = document.getElementById(id);
         if (section) {
           section.scrollIntoView({ behavior: 'smooth' });
-          closeNav(); // Close the mobile navigation after scrolling
+          closeNav(); 
         }
     };
   return (
     <div className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[100000] bg-[#09101a]`}>
         <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
-            <div className='nav-link-mobile' onClick={() => scrollToSection('#')}>HOME</div>
+            <div className='nav-link-mobile' onClick={() => scrollToSection('home')}>HOME</div>
             <div className='nav-link-mobile' onClick={() => scrollToSection('services')}>SERVICES</div>
             <div className='nav-link-mobile' onClick={() => scrollToSection('about')}>ABOUT</div>
             <div className='nav-link-mobile' onClick={() => scrollToSection('projects')}>PROJECTS</div>
